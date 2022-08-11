71b55f8a59b79e7446c701db25c25ae462316ebd
Colby Pate <cbpate08@gmail.com>
2022-08-11 14:44:27 -0500

    Add project files.

---
+++ OperatorExercise/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace OperatorExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number: ");
+            var a = int.Parse(Console.ReadLine());  
+
+            Console.WriteLine("Enter a number: ");
+            var b = int.Parse(Console.ReadLine());
+
+            var quotient = a / b;
+            var remainder = a % b;
+            Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
+
+        }
+    }
+}
