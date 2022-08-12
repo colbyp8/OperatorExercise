9016b9c6f81172d8404a0871c342129883a07dec
Colby Pate <cbpate08@gmail.com>
2022-08-12 14:01:38 -0500

    finished project

--- OperatorExercise/Program.cs
+++ OperatorExercise/Program.cs
@@ -7,7 +7,7 @@ namespace OperatorExercise
         static void Main(string[] args)
         {
             Console.WriteLine("Enter a number: ");
-            var a = int.Parse(Console.ReadLine());  
+            var a = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Enter a number: ");
             var b = int.Parse(Console.ReadLine());
@@ -16,6 +16,16 @@ namespace OperatorExercise
             var remainder = a % b;
             Console.WriteLine($"{a}/{b} is {quotient} remainder {remainder}");
 
+            Console.WriteLine("Enter the radius of the circle: ");
+            var radius = double.Parse(Console.ReadLine());
+            var area = AreaOfCircle(radius);
+            Console.WriteLine($"The area of a circle with radius: {radius} is {area}.");
+
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return Math.PI * (radius * radius);
         }
     }
 }
